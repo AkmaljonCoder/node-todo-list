fd8f5b40281b4cbb2cbee2e5a126f3e1cb4e4254
AkmaljonCoder <akmaljonazimjonovcoder@gmail.com>
2023-09-20 21:53:42 +0500

    completed

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser'
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
 
